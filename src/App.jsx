04779a7a2bfe115cb8b9b08b6af394c682cfc292
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-08-21 14:19:40 +0200

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const Home = lazy(() => import('./pages/Home'));
+const Movies = lazy(() => import('./pages/Movies'));
+const MovieDetails = lazy(() => import('./pages/MovieDetails'));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies" element={<MovieDetails />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/goit-react-hw-05-movies">
     <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>
-  
 );
