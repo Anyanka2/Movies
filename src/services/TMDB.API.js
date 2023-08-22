f12085488acebb607cc1cce8db3b6233015e624a
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-08-22 17:27:52 +0200

    api requests

--- src/services/TMDB.API.js
+++ src/services/TMDB.API.js
@@ -1,1 +1,44 @@
-API_KEY = '6b52787bc9253c3dcb102231856ebbf2';
+import axios from 'axios';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const TREND_URL = `${BASE_URL}/trending/movie/day`;
+const SEARCH_URL = `${BASE_URL}/search/movie`;
+
+const API_KEY = '6b52787bc9253c3dcb102231856ebbf2';
+
+const params = {
+  params: {
+    api_key: API_KEY,
+    language: 'en-US',
+  },
+};
+
+export const fetchTrendingMovies = async () => {
+  const { data } = await axios.get(`${TREND_URL}`, params);
+  return data;
+};
+
+export const fetchSearchQuery = async movieName => {
+  const { data } = await axios.get(`${SEARCH_URL}?query=${movieName}`, params);
+  return data;
+};
+
+export const fetchMovieDetails = async movie_id => {
+  const { data } = await axios.get(`${BASE_URL}/movie/${movie_id}`, params);
+  return data;
+};
+
+export const fetchMovieCast = async movie_id => {
+  const { data } = await axios.get(
+    `${BASE_URL}/movie/${movie_id}/credits`,
+    params
+  );
+  return data;
+};
+export const fetchMovieRewiews = async movie_id => {
+  const { data } = await axios.get(
+    `${BASE_URL}/movie/${movie_id}/rewiews`,
+    params
+  );
+  return data;
+};
