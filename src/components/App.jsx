0a09b8d45c715f81e148e46516d2ec38a91efa0e
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-08-22 22:46:49 +0200

    updates

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import { React, lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const Home = lazy(() => import('../pages/HomePage'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+
+export const App = () => {
+  
+  return (
+    <div>
+      <Routes>
+       <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />} />
+        <Route path="*" element={<Home />} /> 
+      </Routes>
+    </div>
+  );
+};
