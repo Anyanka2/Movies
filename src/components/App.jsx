fbf54ebba9c36a2cefc376e59bef02e9027e7366
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-08-29 00:08:39 +0200

    edit app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,3 @@
-// import { React, lazy } from 'react';
-// import { Route, Routes } from 'react-router-dom';
-
-// const Home = lazy(() => import('../pages/HomePage'));
-// const Movies = lazy(() => import('../pages/MoviesPage'));
-// const MovieDetails = lazy(() => import('../pages/MovieDetailsPage'));
-
-// export const App = () => {
-  
-//   return (
-//     <div>
-//       <Routes>
-//        <Route path="/" element={<Home />} />
-//         <Route path="/movies" element={<Movies />} />
-//         <Route path="/movies/:movieId" element={<MovieDetails />} />
-//         <Route path="*" element={<Home />} /> 
-//       </Routes>
-//     </div>
-//   );
-// };
 import React, { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout/SharedLayout';
